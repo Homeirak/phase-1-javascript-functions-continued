d4dbb653a4f5a5e16bc49859dae5a84b4a0eb3d4
homeira kalim <homeirakalim@gmail.com>
2025-01-30 03:05:05 -0500

    functions continued lab -inner functions- complete

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // code your solution here
+//define function saturdayFun
+//returns "This Saturday, I want to (activity)!" with activity passed in as an argument during function invocation
+//if no argument is passed, returns default activity of roller-skate
+
+function saturdayFun(activity = "roller-skate"){
+    return `This Saturday, I want to ${activity}!`
+}
+console.log(saturdayFun());
+console.log(saturdayFun("read"));
+
+// 
+const mondayWork = function(activity = "go to the office"){
+    return `This Monday, I will ${activity}.`
+}
+console.log(mondayWork());
+console.log(mondayWork("will work from home"));
+
+//define a function wrapAdjective
+//returns "You are ...!" with ... = adjective passed as argument
+//if no argument is passed, return "You are special!""
+
+function wrapAdjective(highlight){
+    return function(adjective = "special"){
+            return `You are ${highlight}${adjective}${highlight}!` 
+        
+    }
+}
+    
+console.log(wrapAdjective("*"),("a hard worker"));
+console.log(wrapAdjective("||"),("a dedicated programmer"));
